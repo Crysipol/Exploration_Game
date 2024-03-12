1ad0d35a951db733bac5969783994d19e347c729
Kevin Paute <Kevin.Paute2@gmail.com>
2024-03-12 08:03:30 -0400

    Update EnemyAI.cs

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public GameObject player;
+    public float speed;
+
+    private float distance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction  = player.transform.position - transform.position;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+
+
     }
 }
