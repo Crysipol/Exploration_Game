97b09cd87a564c3daf634de1f9ae3d27ed987cd4
Kevin Paute <Kevin.Paute2@gmail.com>
2024-03-21 17:42:19 -0400

    Test Animations

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,7 @@ public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
     public float speed;
-
+    public int health = 30;
     private float distance;
 
     // Start is called before the first frame update
@@ -15,14 +15,20 @@ public class EnemyAI : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction  = player.transform.position - transform.position;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerProjectile")
+        {
+            distance = Vector2.Distance(transform.position, player.transform.position);
+            Vector2 direction  = player.transform.position - transform.position;
 
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
     }
 }
