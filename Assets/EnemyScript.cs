4b83da8317e1e41650778a0ea3bd52f8e521e147
Kevin Paute <Kevin.Paute2@gmail.com>
2024-03-29 12:55:29 -0400

    Fix enemy view script

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoblinScript : MonoBehaviour
+public class EnemyScript : MonoBehaviour
 {
     public float radius = 5f;
-    [Range(1, 180)]public float angle = 45f; 
+    [Range(90,270)]public float angle = 60f; 
     public LayerMask targetLayer;
     public LayerMask obstructionLayer;
     public GameObject playerRef;
@@ -37,17 +37,38 @@ public class GoblinScript : MonoBehaviour
             if(Vector2.Angle(transform.up, directionToTarget) < angle / 2){
                 float distanceToTarget = Vector2.Distance(transform.position, target.position);
                 
-                if(Physics2D.Raycasting(transform.position, directionToTarget, distanceToTarget, obstructionLayer)){
+                if(Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionLayer))
                     CanSeePlayer = true;
-                }
                 else
                     CanSeePlayer = false;
             }
             else
                 CanSeePlayer = false;
         }
-        else if (CanSeePlayer){
+        else if (CanSeePlayer)
             CanSeePlayer = false;
+    }
+
+    private void OnDrawGizmos(){
+        Gizmos.color = Color.white;
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, radius);
+
+        Vector3 angle01 = DirectionFromAngle(-transform.eulerAngles.z, -angle / 2);
+        Vector3 angle02 = DirectionFromAngle(-transform.eulerAngles.z, angle / 2);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + angle01 * radius);
+        Gizmos.DrawLine(transform.position, transform.position + angle02 * radius);
+
+        if (CanSeePlayer){
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, playerRef.transform.position);
         }
     }
+
+    private Vector2 DirectionFromAngle(float eulerY, float angleInDegrees){
+        angleInDegrees += eulerY;
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
 }
